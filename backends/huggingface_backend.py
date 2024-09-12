df2403e5ee8ca1f8601182fdad5eed1e98ad5d2b
momentino <filippomomente@protonmail.com>
2024-09-12 17:35:50 +0200

    - fixed issue with temperature

--- backends/huggingface_backend.py
+++ backends/huggingface_backend.py
@@ -13,6 +13,10 @@ class HuggingfaceBackend:
 
         self.max_tokens = max_tokens
         self.temperature = temperature
+        # check if model's generation_config has pad_token_id set:
+        if not self.model.generation_config.pad_token_id:
+            # set pad_token_id to tokenizer's eos_token_id to prevent excessive warnings:
+            self.model.generation_config.pad_token_id = self.tokenizer.eos_token_id
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
     def chat(self, prompt, system=None):
@@ -35,7 +39,6 @@ class HuggingfaceBackend:
         input_length = message_tokens.shape[-1]
         generated_ids = self.model.generate(
             message_tokens,
-            temperature=self.temperature,
             max_new_tokens=self.max_tokens,
             do_sample=False
         )
