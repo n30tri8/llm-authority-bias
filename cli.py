7391a1336290e7c1aec2cf3fdb6f71606be28bca
n30tri8 <n30tri8@gmail.com>
2025-04-27 07:58:35 +0200

    Refactor extract known questions into a separate routine.

--- cli.py
+++ cli.py
@@ -7,7 +7,7 @@ from experiments.analyze.correlation import correlation
 from utils.utils import get_results_path, create_results_file
 from utils.logger import file_logger, out_logger
 from experiments.measure.measure import measure
-from experiments.measure.get_known_questions import get_known_questions
+from experiments.measure.get_known_questions import get_known_questions, get_qbank_known_dir
 from experiments.analyze.summary import summarize
 from backends import get_model
 from pathlib import Path
@@ -17,6 +17,13 @@ PROJECT_ROOT = Path(__file__).resolve().parent
 def get_args_parser():
     parser = argparse.ArgumentParser('', add_help=False)
     sub_parsers = parser.add_subparsers(dest="command_name")
+
+    measure_parsers = sub_parsers.add_parser("extract-known")
+    measure_parsers.add_argument('--qbank', type=str, required=True)
+    measure_parsers.add_argument('--model', type=str, required=True)
+    measure_parsers.add_argument('--max_tokens', type=int, default=512)
+    measure_parsers.add_argument('--temperature', type=float, default=0)
+
     measure_parsers = sub_parsers.add_parser("measure")
     measure_parsers.add_argument('--qbank', type=str, required=True)
     measure_parsers.add_argument('--model', type=str, required=True)
@@ -39,22 +46,56 @@ def get_args_parser():
     correlation_compute.add_argument('--first_person', action='store_true')
     return parser
 
+def load_model(model_name, max_tokens, temperature):
+    model_registry = json.load(open(PROJECT_ROOT / "backends/model_registry.json", 'r'))
+    model_dict = next((item for item in model_registry if item.get('model_name') == model_name), None)
+    gen_args = {
+        'max_tokens': max_tokens,
+        'temperature': temperature,
+    }
+    model = get_model(model_dict, gen_args)
+    return model
+
+
 def main(args):
-    if args.command_name == "measure":
+    if args.command_name == "extract-known":
         model_name = args.model
-        model_registry = json.load(open(PROJECT_ROOT / "backends/model_registry.json", 'r'))
-        model_dict = next((item for item in model_registry if item.get('model_name') == model_name), None)
-        gen_args = {
-            'max_tokens': args.max_tokens,
-            'temperature': args.temperature,
-        }
-        log_msg = f"Loading model from registry with name {model_name}, and gen_args: {gen_args}"
+        max_tokens = args.max_tokens
+        temperature = args.temperature
+        model = load_model(model_name, max_tokens, temperature)
+        log_msg = f"Loading model from registry with name {model_name}"
         file_logger.info(log_msg)
         out_logger.info(log_msg)
-        model = get_model(model_dict, gen_args)
 
         """ Convert the question bank file into a dataframe """
         qbank_df = pd.read_csv(args.qbank)
+
+        if (f'{model_name}-known' not in qbank_df.columns) or (f'{model_name}-fullanswer' not in qbank_df.columns):
+            log_msg = f"I cannot find the known questions or the log of the answer provided by models to such questions. Retrieving the questions for which the model knows the answer.."
+            file_logger.info(log_msg)
+            out_logger.info(log_msg)
+            get_known_questions(model=model, qbank=qbank_df, qbank_dir=args.qbank)
+        log_msg = f"The questions known by the model were extracted."
+        file_logger.info(log_msg)
+        out_logger.info(log_msg)
+
+    if args.command_name == "measure":
+        model_name = args.model
+        max_tokens = args.max_tokens
+        temperature = args.temperature
+        model = load_model(model_name, max_tokens, temperature)
+        log_msg = f"Loading model from registry with name {model_name}"
+        file_logger.info(log_msg)
+        out_logger.info(log_msg)
+
+        """ Convert the question bank file into a dataframe """
+        qbank_known_dir = get_qbank_known_dir(model_name, args.qbank)
+        qbank_df = pd.read_csv(qbank_known_dir)
+        if (f'{model_name}-known' not in qbank_df.columns) or (f'{model_name}-fullanswer' not in qbank_df.columns):
+            log_msg = f"I cannot find the known questions or the log of the answer provided by models to such questions. Extract them first."
+            file_logger.info(log_msg)
+            out_logger.info(log_msg)
+            raise ValueError("The model does not know the questions. Please run the extract-known command first.")
 
         profession = args.profession
         workplace_study = args.workplace_study
@@ -70,15 +111,7 @@ def main(args):
         log_msg = f"First person? [{first_person}]"
         file_logger.info(log_msg)
         out_logger.info(log_msg)
-        if (f'{model_name}-known' not in qbank_df.columns) or (f'{model_name}-fullanswer' not in qbank_df.columns):
-            log_msg = f"I cannot find the known questions or the log of the answer provided by models to such questions. Retrieving the questions for which the model knows the answer.."
-            file_logger.info(log_msg)
-            out_logger.info(log_msg)
-            qbank_root = args.qbank[:args.qbank.rfind('/')]
-            get_known_questions(model=model, qbank=qbank_df, qbank_root=qbank_root)
-        log_msg = f"The questions known by the model are available. Proceeding.."
-        file_logger.info(log_msg)
-        out_logger.info(log_msg)
+
         measure(model=model, qbank=qbank_df, profession=profession, workplace_study=workplace_study, position=position, gender=gender, first_person=first_person, results_file=results_file)
         log_msg = "Experiment completed."
         file_logger.info(log_msg)
@@ -94,6 +127,7 @@ def main(args):
         if not os.path.exists(results_path):
             os.makedirs(results_path)
         correlation(results_path=results_path, first_person=args.first_person)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(parents=[get_args_parser()])
--- experiments/measure/get_known_questions.py
+++ experiments/measure/get_known_questions.py
@@ -1,16 +1,26 @@
 import os
-from utils.logger import out_logger, file_logger
+
 from tqdm import tqdm
 
+from utils.logger import out_logger, file_logger
 
 
-def get_known_questions(model, qbank, qbank_root):
+def get_qbank_known_dir(model_name, qbank):
+    filename = f'qbank_known_{model_name}.csv'
+    qbank_root = qbank[:qbank.rfind('/')]
+    dir_name = os.path.join(qbank_root, filename)
+    return dir_name
+
+
+def get_known_questions(model, qbank, qbank_dir):
     tqdm.pandas()
     seen_question_blocks = set()
     total_answers = []
     full_answers = []
-    for index, row in tqdm(qbank.iterrows(), total=len(qbank), desc="Assessing model's perfromance on the question bank"):
-        question = {'role':'user', 'content':f'Question: {row["question"]}\nPossible answers:\na:{row["a"]}\nb:{row["b"]}\nc:{row["c"]}\nd:{row["d"]}\ne:{row["e"]}'}
+    for index, row in tqdm(qbank.iterrows(), total=len(qbank),
+                           desc="Assessing model's perfromance on the question bank"):
+        question = {'role': 'user',
+                    'content': f'Question: {row["question"]}\nPossible answers:\na:{row["a"]}\nb:{row["b"]}\nc:{row["c"]}\nd:{row["d"]}\ne:{row["e"]}'}
         if row['block'] not in seen_question_blocks and row['block'] != -1 and row['category'] != 'Psychiatry':
             seen_question_blocks.add(row['block'])
             """ Answer the question """
@@ -18,9 +28,10 @@ def get_known_questions(model, qbank, qbank_root):
             file_logger.info(f"Question index: {index}: ")
             file_logger.info(f"QUESTION: {question['content']}")
             file_logger.info(f"ANSWER: {raw_answer}")
-            answer = {'role':'assistant', 'content':raw_answer}
+            answer = {'role': 'assistant', 'content': raw_answer}
             """ Get the answer in a more uniformed format by asking to extract it from the previous answer"""
-            prompt_format_answer = {'role': 'user', 'content': "I'm sorry I didn't understand what was final answer. Please extract the letter corresponding to the answer among a,b,c,d,e. Answer ONLY with the appropriate letter and nothing else."}
+            prompt_format_answer = {'role': 'user',
+                                    'content': "I'm sorry I didn't understand what was final answer. Please extract the letter corresponding to the answer among a,b,c,d,e. Answer ONLY with the appropriate letter and nothing else."}
             prompt = ([question, answer, prompt_format_answer])
             formatted_answer = model.chat(prompt=prompt)
             if formatted_answer == row['answer']:
@@ -35,11 +46,10 @@ def get_known_questions(model, qbank, qbank_root):
                 total_answers.append(question_known)
                 full_answers.append("")
         else:
-            total_answers.append(-1) # It doesn't even try to answer
+            total_answers.append(-1)  # It doesn't even try to answer
             full_answers.append("")
-    qbank[model.model_name+'-known'] = total_answers
+    qbank[model.model_name + '-known'] = total_answers
     qbank[model.model_name + '-fullanswer'] = full_answers
-    qbank.to_csv(os.path.join(qbank_root, "question_bank.csv"), index=False)
+    qbank_known_dir = get_qbank_known_dir(model.model_name, qbank_dir)
+    qbank.to_csv(qbank_known_dir, index=False)
     out_logger.info("QBank updated.")
-
-
